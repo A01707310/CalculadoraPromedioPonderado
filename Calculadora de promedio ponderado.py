7502529020610af5e504c65392ee0f09feefc76f
A01707310 <a01707310@tec.mx>
2024-10-17 23:47:57 -0600

    Update Calculadora de promedio ponderado.py
    Se añadió la función que guarda los promedios ponderados de cada materia dentro de un archivo de texto dentro de la misma carpeta en que se encuentra el código, funciona sólo si no hubo excepciones. Se corrigió el estilo basándonos en pep8. Reestructuración y un poco de limpieza en el código.

--- Calculadora de promedio ponderado.py
+++ Calculadora de promedio ponderado.py
@@ -1,68 +1,92 @@
 """
 **Situación problema**
-Tienes cuatro clases: Matemáticas, Física, Química y Literatura. Cada una tiene diferentes ponderaciones (Matemáticas 40%, Física 30%, Química 20% y Literatura 10%).
-Usando una calculadora de promedio ponderado, deberás ingresar tus calificaciones de cada materia y las ponderaciones correspondientes para obtener tu promedio final,
-que podría guardarse en un registro.
+Tienes tres clases: Matemáticas, Física y Química y quieres conocer el
+promedio ponderado para cada una, usando esta calculadora de promedio
+ponderado, deberás ingresar las notas de cada materia y las ponderaciones
+correspondientes a dichas notas para obtener tu promedio ponderado de cada
+materia, que podría guardarse en un registro.
 """
 
-def list_materias() -> list[str]:
+def obtener_materias() -> list[str]:
     """Permite ingresar materias hasta que el usuario ingrese 'LISTO'"""
+    materias = []
+    entrada = input("INGRESA el nombre de una materia para ponderar sus \
+notas: ")
+    while entrada != "LISTO":
+        materias.append(entrada)
+        entrada = input("INGRESA otra materia o avanzar ingresando 'LISTO': ")
+    print("\n")
+    return materias
     
-    L_materias = []
-    cnt = 0
-
-    Nombre_Materia = input("Ingresa el nombre de una materia para ponderar sus notas: ")
-
-    while Nombre_Materia not in ["LISTO", "listo", "Listo", "lISTO"]:
-        cnt += 1
-        L_materias.append(f"Materia {cnt}: {Nombre_Materia}")
-        Nombre_Materia = input("Ingresar otra materia o avanzar ingresando 'LISTO': ")
-    print("\n\n")
-    return L_materias
-    
-def obtener_num_de_notas(materias:list) -> list[str]:
-    """Pregunta al usuario el número de notas para cada materia, en orden de la lista de materias"""
+def obtener_num_de_notas(materias: list) -> list[str]:
+    """Pregunta al usuario el número de notas para cada materia, en orden de
+    la lista de materias"""
 
     sorteado_materias_num_notas = []
     
     for materia in materias:
-        n_notas = int(input(f'Ingresa el número de notas en "{materia}": '))
-        print("\n")
+        n_notas = int(input(f'INGRESA el número de notas en "{materia}": '))
         sorteado_materias_num_notas.append(n_notas)
- 
+    print("\n")
     return sorteado_materias_num_notas
 
 def promediar_materia(n_notas: int, materia: str) -> float:
-    """
-    Función que dada una cantidad de notas de una materia, registra para cada una su ponderación y la calificación correspondiente.
-    Devuelve el promedio ponderado a partir de dicha información.
-    """
+    """Dada una cantidad de notas, registra una ponderación y una
+    calificación correspondiente para cada una. Devuelve el
+    promedio ponderado a partir de estos registros."""
 
     total_ponderacion = 0
     suma_ponderada = 0
 
-    # Esta condicional evita que se haga una división por cero cuando se especifica la cantidad de 0 notas para una materia
+    # Esta condicional evita que se haga una división por cero cuando se
+    # especifica la cantidad de 0 notas para una materia
     if n_notas > 0:
         # Usamos listas en lugar de tuplas
         ponderaciones_calificaciones = [
-            [
-                float(input(f'Ingresa la ponderación decimal de la nota {i + 1}, en la materia "{materia}"')),
-                float(input(f"Ingresa la calificación de 0 a 100 que fue obtenida para esta nota: "))
-            ] for i in range(n_notas)
-        ]
+            [float(input(f'INGRESA la ponderación decimal (0.0 - 1.0) de \
+la nota {i+1} de {n_notas}, en la materia "{materia}": ')),
+            float(input(f"INGRESA la calificación de 0 a 100, obtenida \
+para esta nota: "))]
+            for i in range(n_notas)]
         
         # Cálculo de la suma ponderada y el total de ponderaciones
-        suma_ponderada = sum([nota[0] * nota[1] for nota in ponderaciones_calificaciones])
-        total_ponderacion = sum([nota[0] for nota in ponderaciones_calificaciones])
+        suma_ponderada = sum([nota[0] * nota[1] for nota in\
+        ponderaciones_calificaciones])
+
+        total_ponderacion = sum([nota[0] for nota in \
+        ponderaciones_calificaciones])
 
         promedio_ponderado = suma_ponderada / total_ponderacion
         return f"{promedio_ponderado:.2f}"
     else:
         return 0.0
 
-#Echando a andar el código, probablemente agreguemos una interfaz tkinter después, para hacerlo más intuitivo.
-lista_De_Materias = list_materias()
-lista_de_N_de_Notas = obtener_num_de_notas(lista_De_Materias)
-for i in range(len(lista_de_N_de_Notas)):
-    print(f'"{lista_De_Materias[i]}" tiene un promedio ponderado de {promediar_materia(n_notas=lista_de_N_de_Notas[i], materia=lista_De_Materias[i])}')
-    print("\n")
+def guardar_promedios(materias: list, promedios: list):
+    """Función que intenta registrar los promedios correspondientes
+    a cada calificación en un archivo dentro de la misma carpeta,
+    en caso de excepción, avisa dentro de la terminal"""
+    try:
+        with open('promedios.txt', 'w', encoding="utf-8") as archivo:
+            suma = 0
+            for i in promedios:
+                suma += float(i)
+            suma = suma / len(promedios)
+            for materia, promedio in zip(materias, promedios):
+                archivo.write(f'{materia}: {promedio}\n')
+            archivo.write(f"Promedio General: {suma}")
+        print("Promedios guardados en 'promedios.txt'")
+        archivo.close()
+    except:
+        print("Hubo un error al registrar tu(s) promedio(s) dentro de un\
+archivo.")
+
+lista_materias = obtener_materias()
+lista_n_notas = obtener_num_de_notas(lista_materias)
+lista_promedios = []
+
+#iteramos sobre la lista de número de notas por materia
+for i in range(len(lista_n_notas)):
+    promedio = promediar_materia(lista_n_notas[i],lista_materias[i])
+    print(f'"{lista_materias[i]}", promedio ponderado de {promedio}\n')
+    lista_promedios.append(promedio)
+guardar_promedios(lista_materias, lista_promedios)
